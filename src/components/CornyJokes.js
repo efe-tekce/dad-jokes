1f44a6a5bb81e8f48538adbc46bfe32726dc0cbc
-- <eefe.tekce@gmail.com>
2022-01-09 19:57:49 +0100

    style changes

--- src/components/CornyJokes.js
+++ src/components/CornyJokes.js
@@ -15,7 +15,7 @@ export const CornyJokes = () => {
   };
   return (
     <div className="p-12 md:p-24 w-3/4 max-h-[80%] md:mx-auto md:w-2/3 bg-neutral-300 bg-opacity-20 rounded-xl shadow-2xl border border-solid border-emerald-200 border-opacity-10 flex flex-col justify-around items-center absolute space-y-12 md:space-y-24 ">
-      <p className="text-center text-xl md:text-2xl font-semibold text-teal-800">
+      <p className="text-center text-xl md:text-2xl font-semibold text-teal-800 shade">
         Here are some corny dad jokes to make your day!
       </p>
       <p className="text-center text-2xl overflow-y-scroll md:text-3xl font-bold text-indigo-700">
